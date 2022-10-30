333a02d454df86f3ef44ec81a773b18cc2b32016
NOWEN <>
2022-10-30 23:39:19 +0530

    Admin portal

--- app/src/main/java/com/example/innova/Admin.java
+++ app/src/main/java/com/example/innova/Admin.java
@@ -1,14 +1,43 @@
 package com.example.innova;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Admin extends AppCompatActivity {
+
+    Button btnBack,btnCal;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_admin2);
+
+
+        btnBack =(Button) findViewById(R.id.btnBack);
+        btnCal =(Button) findViewById(R.id.btnCal);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ProductMainActivity.class));
+
+            }
+        });
+
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AdminCalculation.class));
+
+            }
+        });
+
     }
 }
